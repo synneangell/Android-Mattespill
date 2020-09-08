586fe6a70bd7e5a31b52fe84787fb55ef35cc94c
Nikola Sekiw <nnikolasekiw@users.noreply.github.com>
2020-09-08 17:44:03 +0200

    Fikset på layout og fikset slik at alle strings, toast og lignende også er på tysk + la til en alert om brukeren vil gå ut av spillet før det er ferdig

--- app/src/main/java/mattespill/com/example/myapplication/StartSpill.java
+++ app/src/main/java/mattespill/com/example/myapplication/StartSpill.java
@@ -55,7 +55,6 @@ public class StartSpill extends AppCompatActivity {
         textOppgaverIgjen = (TextView)findViewById(R.id.textOppgaverIgjen);
         random = new Random();
 
-
         //valgtRadiobutton();
         randomGenerator();
 
